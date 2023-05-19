5091d2fb5292ea9b2d47e4ed3f2e57956f43829e
Victor-UGB <76266429+Victor-UGB@users.noreply.github.com>
2023-05-19 02:30:51 +0100

    new commit

--- eatfoodbackend/api/views.py
+++ eatfoodbackend/api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from rest_framework.response import Response
+from rest_framework import generics, status
 from rest_framework.decorators import api_view
+from .models import Vendor
+from .serializers import VendorSerializer
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
@@ -9,3 +13,25 @@ def home(request):
     return Response({
         "data": " Hello From Django Backend"
     })
+
+class VendorsView(generics.ListAPIView):
+    queryset = Vendor.objects.all()
+    serializer_class = VendorSerializer
+
+@api_view(["GET", "POST"])
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return Response({'Message': 'Login Success'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'Message': 'Login Failed'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        return Response({'Message': 'Login Request'} )
